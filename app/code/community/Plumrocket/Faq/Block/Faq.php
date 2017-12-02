e03923672eec9cbd02dd9efcf80f242b6238af30
volodimyrholovatskiy <volodimyrholo@gmail.com>
2017-12-02 08:22:44 +0200

    add faq list to frontend page

--- app/code/community/Plumrocket/Faq/Block/Faq.php
+++ app/code/community/Plumrocket/Faq/Block/Faq.php
@@ -1,5 +1,12 @@
 <?php
- class Plumrocket_Faq_Block_Faq extends Mage_Core_Block_Template
- {
-   
- }
+class Plumrocket_Faq_Block_Faq extends Mage_Core_Block_Template
+{
+    public function getFaqlist()
+    {
+        $collection = Mage::getModel('faq/block')->getCollection()
+                                                 ->addFieldToFilter('faq_status', '1')
+                                                 ->setOrder('title', 'asc');
+
+        return $collection->getdata();
+    }
+}
