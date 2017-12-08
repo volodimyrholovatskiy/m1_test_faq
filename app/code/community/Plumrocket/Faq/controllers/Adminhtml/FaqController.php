9972c3a4c2cca883bbd12ac2d915c82020c11670
volodimyrholovatskiy <volodimyrholo@gmail.com>
2017-12-08 15:44:58 +0200

    fix remark 5

--- app/code/community/Plumrocket/Faq/controllers/Adminhtml/FaqController.php
+++ app/code/community/Plumrocket/Faq/controllers/Adminhtml/FaqController.php
@@ -2,49 +2,41 @@
 
 class Plumrocket_Faq_Adminhtml_FaqController extends Mage_Adminhtml_Controller_Action
 {
-    protected function _isAllowed()
-	  {
-		    //return Mage::getSingleton('admin/session')->isAllowed('faq/faqbackend');
-		    return true;
-	  }
 
     protected function _initAction()
     {
         $this->loadLayout()
-             ->_setActiveMenu('plumrocket');
+             ->_setActiveMenu('plumrocket')
+             ->_title($this->__('Plumrocket'))
+             ->_title($this->__('FAQ'))
+             ->_title($this->__('Manage FAQ'));
         return $this;
     }
 
 	  public function indexAction()
     {
-        $this->_title($this->__('Plumrocket'))
-             ->_title($this->__('FAQ'))
-             ->_title($this->__('Manage FAQ'));
         $this->_initAction();
 	      $this->renderLayout();
     }
 
     public function editAction()
     {
-        $id = $this->getRequest()->getParam('faq_id');
+        $id = intval($this->getRequest()->getParam('faq_id'));
 
         $model = Mage::getModel('faq/block')->load($id);
 
         if ($id) {
             if (! $model->getId() ) {
-                Mage::getSingleton('adminhtml/session')->addError(Mage::helper('faq')->__('This faq no longer exists.'));
+                $this->_getSession()->addError($this->__('This faq no longer exists.'));
                 $this->_redirect('*/*/');
                 return;
             }
         }
 
-        Mage::register('faq_block',Mage::getModel('faq/block')->load($id));
+        Mage::register('faq_block', Mage::getModel('faq/block')->load($id));
 
-        $this->_title($this->__('Plumrocket'))
-             ->_title($this->__('FAQ'))
-             ->_title($this->__('Manage FAQ'));
-        $this->_title($model->getId() ? $model->getTitle() : $this->__('New Faq'));
-        $this->_initAction();
+        $this->_initAction()
+             ->_title($model->getId() ? $model->getTitle() : $this->__('New Faq'));
         $this->renderLayout();
     }
 
@@ -65,11 +57,11 @@ class Plumrocket_Faq_Adminhtml_FaqController extends Mage_Adminhtml_Controller_A
                       ->setFaq_status($postData['faq_status'])
                       ->setContent($postData['content'])
                       ->save();
-                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('faq')->__('FAQ was successfully saved'));
+                $this->_getSession()->addSuccess($this->__('FAQ was successfully saved'));
                 $this->_redirect('*/*/');
                 return;
             } catch (Exception $e) {
-                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                $this->_getSession()->addError($e->getMessage());
                 $this->_redirect('*/*/edit', array('faq_id' => $this->getRequest()->getParam('faq_id')));
                 return;
             }
@@ -80,19 +72,18 @@ class Plumrocket_Faq_Adminhtml_FaqController extends Mage_Adminhtml_Controller_A
 
     public function deleteAction()
     {
-        if ( $id = $this->getRequest()->getParam('faq_id') ) {
+        if ( $id = intval($this->getRequest()->getParam('faq_id')) ) {
             try {
-                $model = Mage::getModel('faq/block');
-                $model->load($id);
+                $model = Mage::getModel('faq/block')->load($id);
                 $title = $model->getTitle();
                 $model->delete();
 
-                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('faq')->__('Faq was deleted successfully!'));
+                $this->_getSession()->addSuccess($this->__('Faq was deleted successfully!'));
 
                 $this->_redirect('*/*/');
                 return;
             } catch (Exception $e) {
-                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                $this->_getSession()->addError($e->getMessage());
 
                 $this->_redirect('*/*/edit', array('faq_id' => $id));
                 return;
@@ -100,7 +91,7 @@ class Plumrocket_Faq_Adminhtml_FaqController extends Mage_Adminhtml_Controller_A
 
         }
 
-        Mage::getSingleton('adminhtml/session')->addError(Mage::helper('faq')->__('Unable to find a faq to delete.'));
+        $this->_getSession()->addError($this->__('Unable to find a faq to delete.'));
 
         $this->_redirect('*/*/');
     }
@@ -110,18 +101,16 @@ class Plumrocket_Faq_Adminhtml_FaqController extends Mage_Adminhtml_Controller_A
         $faqIds = $this->getRequest()->getParam('massaction');
 
         if ( !is_array($faqIds) ) {
-            Mage::getSingleton('adminhtml/session')->addError($this->__('Please select faq(s).'));
+            $this->_getSession()->addError($this->__('Please select faq(s).'));
         } else {
             try {
                 foreach ($faqIds as $faqId) {
                     $faq = Mage::getModel('faq/block')->load($faqId);
                     $faq->delete();
                 }
-                Mage::getSingleton('adminhtml/session')->addSuccess(
-                    $this->__('Total of %d record(s) have been deleted.', count($faqIds))
-                );
+                $this->_getSession()->addSuccess($this->__('Total of %d record(s) have been deleted.', count($faqIds)));
             } catch (Exception $e) {
-                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                $this->_getSession()->addError($e->getMessage());
             }
         }
 
@@ -133,7 +122,7 @@ class Plumrocket_Faq_Adminhtml_FaqController extends Mage_Adminhtml_Controller_A
         $faqIds = $this->getRequest()->getParam('massaction');
 
         if ( !is_array($faqIds) ) {
-            Mage::getSingleton('adminhtml/session')->addError($this->__('Please select faq(s).'));
+            $this->_getSession()->addError($this->__('Please select faq(s).'));
         } else {
             try {
                 foreach ($faqIds as $faqId) {
@@ -141,14 +130,17 @@ class Plumrocket_Faq_Adminhtml_FaqController extends Mage_Adminhtml_Controller_A
                     $faq->setFaq_status($this->getRequest()->getParam('faq_status'));
                     $faq->save();
                 }
-                Mage::getSingleton('adminhtml/session')->addSuccess(
-                    $this->__('Total of %d record(s) have been updated.', count($faqIds))
-                );
+                $this->_getSession()->addSuccess($this->__('Total of %d record(s) have been updated.', count($faqIds)));
             } catch (Exception $e) {
-                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                $this->_getSession()->addError($e->getMessage());
             }
         }
 
         $this->_redirect('*/*/');
+    }
+
+    protected function _getSession()
+    {
+        return Mage::getSingleton('adminhtml/session');
     }
 }
